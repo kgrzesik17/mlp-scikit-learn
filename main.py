e7f22a856a7b27a17e6016ae25f1aa4a30e54cf6
Kacper <kgrzesik17@gmail.com>
2025-12-11 20:51:25 +0100

    algo

--- main.py
+++ main.py
@@ -15,10 +15,39 @@ X = X.to_numpy()
 scaler = MinMaxScaler()
 X = scaler.fit_transform(X)
 
-print(X)
-# print(dataframe)
-
-# y = dataframe['DEATH_EVENT']
-# y = y.to_numpy()
+y = dataframe['diagnosis']
+y = y.to_numpy()
 # print(X.shape)
 # print(y.shape)
+
+print(X)
+
+hidden_layer_sizes = [(50,), (100,), (50,100), (100, 100), (25,), (25,50)]
+scores = {}
+
+for l in hidden_layer_sizes:
+  mlp = MLPClassifier(hidden_layer_sizes=l, random_state=1410, max_iter=400)
+  score = cross_val_score(mlp, X, y, cv=10).mean()
+  scores[l] = score
+  print("warstwy %s dokladnosc: %2.2f" %(str(l), 100*score))
+
+plt.figure(figsize=(20,10))
+plt.ylim([0.7,0.9])
+for k,v in scores.items():
+  plt.bar(str(k),v)
+plt.show()
+
+solvers = ['sgd', 'adam', 'lbfgs']
+scores = {}
+
+for s in solvers:
+  mlp = MLPClassifier(hidden_layer_sizes=(50,), solver=s, random_state=1410, max_iter=1000)
+  score = cross_val_score(mlp, X, y, cv=10).mean()
+  scores[s] = score
+  print("alogrytm %s dokladnosc: %2.2f" %(str(l), 100*score))
+
+  plt.figure(figsize=(20,10))
+plt.ylim([0.5,0.9])
+for k,v in scores.items():
+  plt.bar(str(k),v)
+plt.show()
